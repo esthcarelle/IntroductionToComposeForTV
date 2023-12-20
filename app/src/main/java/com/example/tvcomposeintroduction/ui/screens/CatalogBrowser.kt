f17b053bb2d66ccd1422810ed8ffba73c6c7c273
Esther Carrelle <esthcarelle@gmail.com>
2023-12-20 20:11:27 +0200

    Add a screen transition to the details screen

--- app/src/main/java/com/example/tvcomposeintroduction/ui/screens/CatalogBrowser.kt
+++ app/src/main/java/com/example/tvcomposeintroduction/ui/screens/CatalogBrowser.kt
@@ -1,29 +1,15 @@
-/*
- * Copyright 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.example.tvcomposeintroduction.ui.screens
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.tv.foundation.lazy.list.TvLazyColumn
@@ -32,8 +18,11 @@ import androidx.tv.foundation.lazy.list.items
 import androidx.tv.material3.Carousel
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Text
+import coil.compose.AsyncImage
+import com.example.tvcomposeintroduction.R
 import com.example.tvcomposeintroduction.data.Movie
 import com.example.tvcomposeintroduction.ui.components.MovieCard
+import com.example.tvcomposeintroduction.ui.screens.CatalogBrowserViewModel
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
@@ -44,7 +33,6 @@ fun CatalogBrowser(
 ) {
     val categoryList by
     catalogBrowserViewModel.categoryList.collectAsState()
-
     TvLazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -56,14 +44,27 @@ fun CatalogBrowser(
                 itemCount = featuredMovieList.size,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(376.dp)
-            ) { indexOfCarouselSlide ->
-                val featuredMovie =
-                    featuredMovieList[indexOfCarouselSlide]
+                    .height(376.dp),
+            ) { indexOfCarouselItem ->
+                val featuredMovie = featuredMovieList[indexOfCarouselItem]
+                CarouselSlide(
+                    background = {
+                        AsyncImage(
+                            model = featuredMovie.backgroundImageUrl,
+                            contentDescription = null,
+                            placeholder = painterResource(
+                                id = R.drawable.placeholder
+                            ),
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize(),
+                        )
+                    },
+                    modifier = Modifier.clickable { onMovieSelected(featuredMovie) }
+                ) {
+                    Text(text = featuredMovie.title)
+                }
             }
         }
-
-
         items(categoryList) { category ->
             Text(text = category.name)
             TvLazyRow(
